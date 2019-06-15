609b56da002a0de79570d14f2b9066efcad21188
Alan Van <alancllvan@gmail.com>
2019-06-15 15:12:35 +0700

    second commit.

--- binary_tree/model/Solution.java
+++ binary_tree/model/Solution.java
@@ -1,7 +1,6 @@
 package binary_tree.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
@@ -54,12 +53,12 @@ public class Solution {
     /**
      * Post-order traversal
      */
-    public List<Integer> postorderTraversal(TreeNode root) {
+    public static List<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<>();
         return postorderTraversalHelp(result, root);
     }
 
-    private List<Integer> postorderTraversalHelp(ArrayList<Integer> result, TreeNode root) {
+    private static List<Integer> postorderTraversalHelp(ArrayList<Integer> result, TreeNode root) {
 
         if (root == null) {
             return result;
@@ -73,4 +72,83 @@ public class Solution {
 
         return result;
     }
+
+    /**
+     * Level-order traversal (breadth first search)
+     */
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if (root != null) {
+            q.offer(root);
+        }
+
+        TreeNode current;
+
+        while (!q.isEmpty()) {
+            List<Integer> subResult = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                current = q.poll();
+                if (current != null) {
+                    subResult.add(current.val);
+                    if (current.left != null) {
+                        q.offer(current.left);
+                    }
+                    if (current.right != null) {
+                        q.offer(current.right);
+                    }
+                }
+            }
+            result.add(subResult);
+        }
+        return result;
+    }
+
+    /**
+     * Maximum depth
+     */
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int rightDepth = 0;
+        if (root.right != null) {
+            rightDepth = maxDepth(root.right);
+        }
+
+        int leftDepth = 0;
+        if (root.left != null) {
+            leftDepth = maxDepth(root.left);
+        }
+
+        return Math.max(rightDepth, leftDepth) + 1;
+    }
+
+    /**
+     * Symmetric Tree
+     */
+    public static boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public static boolean isMirror(TreeNode lRoot, TreeNode rRoot) {
+        if (lRoot == null && rRoot == null) {
+            return true;
+        }
+
+        if (lRoot == null || rRoot == null) {
+            return false;
+        }
+
+        return lRoot.val == rRoot.val && isMirror(lRoot.left, rRoot.right) && isMirror(lRoot.right, rRoot.left);
+    }
+//
+//    /**
+//     * Path sum
+//     */
+//    public static boolean hasPathSum(TreeNode root, int sum) {
+//
+//    }
 }
--- binary_tree/test/SolutionTest.java
+++ binary_tree/test/SolutionTest.java
@@ -2,15 +2,57 @@ package binary_tree.test;
 
 import binary_tree.model.Solution;
 import binary_tree.model.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
-    @Test
-    public void testPreOrderTransversal() {
-        TreeNode root = new TreeNode(1);
+    TreeNode root;
+    TreeNode root1;
+    TreeNode root2;
+
+    @BeforeEach
+    void setUp() {
+        root = new TreeNode(1);
         root.right = new TreeNode(2);
-        root.right.left = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root1 = new TreeNode(1);
+        root1.right = new TreeNode(2);
+        root1.left = new TreeNode(2);
+        root1.right.right = new TreeNode(3);
+        root1.left.right = new TreeNode(3);
+
+        root2 = new TreeNode(2);
+        root2.left = new TreeNode(97);
+        root2.right = new TreeNode(97);
+        root2.left.right = new TreeNode(47);
+        root2.right.left = new TreeNode(80);
+        root2.left.right.left = new TreeNode(-7);
+        root2.right.left.right = new TreeNode(-7);
+    }
+
+    @Test
+    public void testTraversal() {
         System.out.println(Solution.preorderTraversal(root));
+        System.out.println(Solution.inorderTraversal(root));
+        System.out.println(Solution.postorderTraversal(root));
+        System.out.println(Solution.levelOrder(root));
+    }
+
+    @Test
+    void testMaxDepth() {
+        System.out.println(Solution.maxDepth(root));
+    }
+
+    @Test
+    void testIsSymmetric() {
+        System.out.println(Solution.isSymmetric(root));
+        System.out.println(Solution.isSymmetric(root1));
+        System.out.println(Solution.isSymmetric(root2));
     }
 }
